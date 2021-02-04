caa3a5b2ede6d3b49e5de5c818bd7622e6d3ea39
Przemyslaw Karcz <przemyslawkarcz@gmail.com>
2021-02-04 14:57:26 +0100

    a main executive class added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+
+import static org.example.DisplayText.displaysOriginalTextMappedToLines;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //if one want to display the whole original text from Text file mapped to number of lines
+        displaysOriginalTextMappedToLines();
+
+
+
+    }
+
+}
