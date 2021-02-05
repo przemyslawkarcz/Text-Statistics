83e2efdfb5b487573f5376aca75b8626f2a8ffc1
Przemyslaw Karcz <przemyslawkarcz@gmail.com>
2021-02-05 10:35:49 +0100

    a new method has been added to the Calculations class named countsCharactersOfText

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Calculations {
@@ -23,6 +25,51 @@ public class Calculations {
         }
 
         System.out.println("No. of lines: " + counter);
+
+    }
+
+    public void countsCharactersOfText() throws FileNotFoundException {
+
+        File originalFile = new File("C:\\Users\\pkarc\\IdeaProjects\\Text-Statistics\\src\\main\\resources\\Text.txt");
+
+        Scanner textFromFileThroughScanner = new Scanner(originalFile);
+
+        Map<Character, Integer> characterIntegerMap = new HashMap<>();
+
+        while (textFromFileThroughScanner.hasNextLine()){
+
+            String lineOfText = textFromFileThroughScanner.nextLine();
+
+            char[] charArray = lineOfText.toCharArray();
+
+            for (int i = 0; i < charArray.length; i++) {
+
+                char c = charArray[i];
+
+                if (!characterIntegerMap.containsKey(charArray[i])){
+
+                    characterIntegerMap.put(charArray[i], 1);
+
+                }else {
+
+                    Integer counter = characterIntegerMap.get(charArray[i]);
+                    counter++;
+                    characterIntegerMap.put(charArray[i], counter);
+                }
+
+            }
+
+        }
+
+        System.out.println("\nK:V");
+        for (Map.Entry<Character, Integer> map:characterIntegerMap.entrySet()) {
+
+            Character key = map.getKey();
+            Integer value = map.getValue();
+
+            System.out.println(key + ":" + value);
+
+        }
 
     }
 
