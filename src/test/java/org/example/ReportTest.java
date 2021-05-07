3c3f1d909c5006b666aa8010e5e59422891c3b91
Karcz Przemysław <pkarc@PGI.LOCAL>
2021-05-07 15:11:26 +0200

    a new ReportTest test class has been added which includes tests for the Report class

---
+++ src/test/java/org/example/ReportTest.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+
+class ReportTest extends TestCase {
+
+    @Test
+    public void testsIfFileExists_CreatesTextStatisticsReport() {
+
+        File file = new File("src/main/resources/Text-Statistics-Report.txt");
+
+        boolean exists = file.exists();
+
+        Assertions.assertTrue(exists);
+
+    }
+
+    @Test
+    public void testsIfFileIsOfClassFile_CreatesTextStatisticsReport() {
+
+        File file = new File("src/main/resources/Text-Statistics-Report.txt");
+
+        boolean isFile = file.isFile();
+
+        Assertions.assertTrue(isFile);
+
+    }
+
+    @Test
+    public void testsIfFileIsNotDirectory_CreatesTextStatisticsReport() {
+
+        File file = new File("src/main/resources/Text-Statistics-Report.txt");
+
+        boolean isDirectory = file.isDirectory();
+
+        Assertions.assertFalse(isDirectory);
+
+    }
+
+    @Test
+    public void testsIfFileIsReadable_CreatesTextStatisticsReport() {
+
+        File file = new File("src/main/resources/Text-Statistics-Report.txt");
+
+        boolean forReading = file.canRead();
+
+        Assertions.assertTrue(forReading);
+
+    }
+
+    @Test
+    public void testsIfFileIsWriteable_CreatesTextStatisticsReport() {
+
+        File file = new File("src/main/resources/Text-Statistics-Report.txt");
+
+        boolean forWriting = file.canWrite();
+
+        Assertions.assertTrue(forWriting);
+
+    }
+
+    @Test
+    public void testsIfFileNameIsCorrect_CreatesTextStatisticsReport() {
+
+        File file = new File("src/main/resources/Text-Statistics-Report.txt");
+
+        String fileName = file.getName();
+
+        Assertions.assertEquals("Text-Statistics-Report.txt", fileName);
+
+    }
+
+}
