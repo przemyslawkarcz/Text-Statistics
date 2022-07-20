ffafdc95da058ad91454660b96cb6b287cf80661
Karcz Przemysław <pkarc@PGI.LOCAL>
2022-07-20 10:15:38 +0200

    added vowel and consonant counting method tests

--- src/test/java/org/example/CalculationsTest.java
+++ src/test/java/org/example/CalculationsTest.java
@@ -216,4 +216,124 @@ class CalculationsTest extends TestCase {
 
     }
 
+    @Test
+    public void testsIfMethodReturnsIntegerValues01_countsNumberOfVowels(){
+
+        String exemplaryText = "Ala has a cat";
+
+        char[] charArray = exemplaryText.toCharArray();
+
+        char[] vowelsArray = {'a', 'A', 'ą', 'Ą', 'e', 'E', 'ę', 'Ę', 'i', 'I', 'o', 'O', 'ó', 'Ó', 'u', 'U', 'y', 'Y'};
+
+        int counterOfVowels = 0;
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            for (int j = 0; j < vowelsArray.length; j++) {
+
+                if (charArray[i] == vowelsArray[j]){
+
+                    counterOfVowels++;
+
+                }
+
+            }
+
+        }
+
+        Assertions.assertEquals(5, counterOfVowels);
+
+    }
+
+    @Test
+    public void testsIfMethodReturnsIntegerValues02_countsNumberOfVowels(){
+
+        String exemplaryText = "Ala has a caterpillar";
+
+        char[] charArray = exemplaryText.toCharArray();
+
+        char[] vowelsArray = {'a', 'A', 'ą', 'Ą', 'e', 'E', 'ę', 'Ę', 'i', 'I', 'o', 'O', 'ó', 'Ó', 'u', 'U', 'y', 'Y'};
+
+        int counterOfVowels = 0;
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            for (int j = 0; j < vowelsArray.length; j++) {
+
+                if (charArray[i] == vowelsArray[j]){
+
+                    counterOfVowels++;
+
+                }
+
+            }
+
+        }
+
+        Assertions.assertNotEquals(5, counterOfVowels);
+
+    }
+
+    @Test
+    public void testsIfMethodReturnsIntegerValues01_countsNumberOfConsonants(){
+
+        String exemplaryText = "Ala has a hat";
+
+        char[] charArray = exemplaryText.toCharArray();
+
+        char[] consonantsArray = {'b', 'B', 'c', 'C', 'ć', 'Ć', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K',
+                'l', 'L', 'ł', 'Ł', 'm', 'M', 'n', 'N', 'ń', 'Ń', 'p', 'P', 'r', 'R', 's', 'S', 'ś', 'Ś', 't', 'T', 'w', 'W',
+                'x', 'X', 'z', 'Z', 'ź', 'Ź', 'ż', 'Ż'};
+
+        int counterOfConsonants = 0;
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            for (int j = 0; j < consonantsArray.length; j++) {
+
+                if (charArray[i] == consonantsArray[j]){
+
+                    counterOfConsonants++;
+
+                }
+
+            }
+
+        }
+
+        Assertions.assertEquals(5, counterOfConsonants);
+
+    }
+
+    @Test
+    public void testsIfMethodReturnsIntegerValues02_countsNumberOfConsonants(){
+
+        String exemplaryText = "Ala meets a hatter";
+
+        char[] charArray = exemplaryText.toCharArray();
+
+        char[] consonantsArray = {'b', 'B', 'c', 'C', 'ć', 'Ć', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K',
+                'l', 'L', 'ł', 'Ł', 'm', 'M', 'n', 'N', 'ń', 'Ń', 'p', 'P', 'r', 'R', 's', 'S', 'ś', 'Ś', 't', 'T', 'w', 'W',
+                'x', 'X', 'z', 'Z', 'ź', 'Ź', 'ż', 'Ż'};
+
+        int counterOfConsonants = 0;
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            for (int j = 0; j < consonantsArray.length; j++) {
+
+                if (charArray[i] == consonantsArray[j]){
+
+                    counterOfConsonants++;
+
+                }
+
+            }
+
+        }
+
+        Assertions.assertNotEquals(7, counterOfConsonants);
+
+    }
+
 }
