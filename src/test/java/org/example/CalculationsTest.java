72038e518b6d790503c8a486e7a9fd137592f220
Karcz Przemysław <pkarc@PGI.LOCAL>
2022-07-20 12:22:01 +0200

    added a test for a method counting uppercase letters

--- src/test/java/org/example/CalculationsTest.java
+++ src/test/java/org/example/CalculationsTest.java
@@ -396,4 +396,63 @@ class CalculationsTest extends TestCase {
 
     }
 
+    @Test
+    public void testsIfMethodReturnsIntegerValues01_countsNumberOfCapitalLetters(){
+
+        String exemplaryText = "It's OK!";
+
+        char[] charArray = exemplaryText.toCharArray();
+
+        int counterOfUppercaseLetters = 0;
+
+        char[] capitalLettersArray = {'Q', 'W', 'E', 'Ę', 'R', 'T', 'Y', 'U', 'I', 'O', 'Ó', 'P', 'A', 'Ą', 'S', 'Ś', 'D', 'F', 'G',
+                'H', 'J', 'K', 'L', 'Ł', 'Z', 'Ź', 'Ż', 'X', 'C', 'Ć', 'V', 'B', 'N', 'Ń', 'M'};
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            for (int j = 0; j < capitalLettersArray.length; j++) {
+
+                if (charArray[i] == capitalLettersArray[j]){
+
+                    counterOfUppercaseLetters++;
+
+                }
+
+            }
+
+        }
+
+        Assertions.assertEquals(3, counterOfUppercaseLetters);
+
+    }
+
+    @Test
+    public void testsIfMethodReturnsIntegerValues02_countsNumberOfCapitalLetters(){
+
+        String exemplaryText = "It's NOT OK!";
+
+        char[] charArray = exemplaryText.toCharArray();
+
+        int counterOfUppercaseLetters = 0;
+
+        char[] capitalLettersArray = {'Q', 'W', 'E', 'Ę', 'R', 'T', 'Y', 'U', 'I', 'O', 'Ó', 'P', 'A', 'Ą', 'S', 'Ś', 'D', 'F', 'G',
+                'H', 'J', 'K', 'L', 'Ł', 'Z', 'Ź', 'Ż', 'X', 'C', 'Ć', 'V', 'B', 'N', 'Ń', 'M'};
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            for (int j = 0; j < capitalLettersArray.length; j++) {
+
+                if (charArray[i] == capitalLettersArray[j]){
+
+                    counterOfUppercaseLetters++;
+
+                }
+
+            }
+
+        }
+
+        Assertions.assertNotEquals(5, counterOfUppercaseLetters);
+
+    }
 }
